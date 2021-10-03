07963fff8a85f2ba4df5dc5b196e859a6e0c5459
ArtakSukiasyan <armen93g@gmail.com>
2021-10-04 03:56:16 +0400

    add value and wile loop

--- The_Secant_Method/The_Secant_Method.py
+++ The_Secant_Method/The_Secant_Method.py
@@ -10,3 +10,33 @@ def F2(x):
 def FSM(xi,xi_1):
     return xi_1-(xi-xi_1)/(F(xi)-F(xi_1))*F(xi_1)
 
+E=1e-3
+a=1
+b=2
+xi=0
+xi_1=0
+
+E=1e-3
+a=1
+b=2
+
+
+xi=0
+xi_1=0
+
+if(F(a)*F2(a)>0):
+    xi=a
+    xi_1=b
+elif(F(b)*F2(b)>0):
+    xi=b
+    xi_1=a
+
+xk=FSM(xi,xi_1)
+while(abs(xk-xi_1)>E):
+    xi_1=xk
+    xk=FSM(xi,xi_1)
+
+
+
+print(xk)
+
