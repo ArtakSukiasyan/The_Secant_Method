9b319380287eff0767a365684a938fdb6ff5a09c
ArtakSukiasyan <armen93g@gmail.com>
2021-10-04 03:49:11 +0400

    add 2 function

--- The_Secant_Method/The_Secant_Method.py
+++ The_Secant_Method/The_Secant_Method.py
@@ -3,3 +3,10 @@ import math as m
 
 def F(x):
    return (m.exp(x))- pow(2,-x)+(2*m.cos(x))-6
+
+def F2(x):
+    return (m.exp(x))-pow(2,-x)*pow(m.log(2),2)+(2*m.cos(x))
+
+def FSM(xi,xi_1):
+    return xi_1-(xi-xi_1)/(F(xi)-F(xi_1))*F(xi_1)
+
